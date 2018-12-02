3bb958d895048bd0a5ca02b89668fb3563148895
natanaelemilio <qualidade.natanael@gmail.com>
2018-12-02 18:24:54 -0200

    nova janela de administração de arquivos

--- SD-Github/src/arquivos/ServerTransfer.java
+++ SD-Github/src/arquivos/ServerTransfer.java
@@ -9,7 +9,6 @@ package arquivos;
  *
  * @author natan
  */
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,80 +16,79 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class ServerTransfer {
-	public static void main(String[] args) {
 
-		// Criando servidor
-		ServerTransfer server = new ServerTransfer();
+    private File file;
 
-		// Aguardar conexao de cliente para transferia
-		server.waitForClient();
-		
-	}
+    public ServerTransfer() {
+    }
 
-	public void waitForClient() {
-		// Checa se a transferencia foi completada com sucesso
-		OutputStream socketOut = null;
-		ServerSocket servsock = null;
-		FileInputStream fileIn = null;
+    public ServerTransfer(File file) {
+        this.file = file;
+    }
 
-		try {
-			// Abrindo porta para conexao de clients
-			servsock = new ServerSocket(13267);
-			System.out.println("Porta de conexao aberta 13267");
+    public void waitForClient() {
+        // Checa se a transferencia foi completada com sucesso
+        OutputStream socketOut = null;
+        ServerSocket servsock = null;
+        FileInputStream fileIn = null;
 
-			// Cliente conectado
-			Socket sock = servsock.accept();
-			System.out.println("Conexao recebida pelo cliente");
+        try {
+            // Abrindo porta para conexao de clients
+            servsock = new ServerSocket(13267);
+            System.out.println("Porta de conexao aberta 13267");
 
-			// Criando tamanho de leitura
-			byte[] cbuffer = new byte[1024];
-			int bytesRead;
+            // Cliente conectado
+            Socket sock = servsock.accept();
+            System.out.println("Conexao recebida pelo cliente");
 
-			// Criando arquivo que sera transferido pelo servidor
-			File file = new File("c:\\chat-app\\teste.zip");
-			fileIn = new FileInputStream(file);
-			System.out.println("Lendo arquivo...");
-			
-			// Criando canal de transferencia
-			socketOut = sock.getOutputStream();
+            // Criando tamanho de leitura
+            byte[] cbuffer = new byte[1024];
+            int bytesRead;
 
-			// Lendo arquivo criado e enviado para o canal de transferencia
-			System.out.println("Enviando Arquivo...");
-			while ((bytesRead = fileIn.read(cbuffer)) != -1) {
-				socketOut.write(cbuffer, 0, bytesRead);
-				socketOut.flush();
-			}
+            // Criando arquivo que sera transferido pelo servidor
+            
+            fileIn = new FileInputStream(file);
+            System.out.println("Lendo arquivo...");
 
-			System.out.println("Arquivo Enviado!");
-		} catch (Exception e) {
-			// Mostra erro no console
-			e.printStackTrace();
-		} finally {
-			if (socketOut != null) {
-				try {
-					socketOut.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+            // Criando canal de transferencia
+            socketOut = sock.getOutputStream();
 
-			if (servsock != null) {
-				try {
-					servsock.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+            // Lendo arquivo criado e enviado para o canal de transferencia
+            System.out.println("Enviando Arquivo...");
+            while ((bytesRead = fileIn.read(cbuffer)) != -1) {
+                socketOut.write(cbuffer, 0, bytesRead);
+                socketOut.flush();
+            }
 
-			if (fileIn != null) {
-				try {
-					fileIn.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+            System.out.println("Arquivo Enviado!");
+        } catch (Exception e) {
+            // Mostra erro no console
+            e.printStackTrace();
+        } finally {
+            if (socketOut != null) {
+                try {
+                    socketOut.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (servsock != null) {
+                try {
+                    servsock.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (fileIn != null) {
+                try {
+                    fileIn.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
